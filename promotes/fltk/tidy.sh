73478867821935c1e9d3bd2f9cdd71412df18393
HmpSoft Admin <humpty@mail.com>
2024-08-29 10:35:21 +0800

    commit

--- promotes/fltk/tidy.sh
+++
@@ -1,5 +0,0 @@
-# if called from elsewhere, make sure we start here
-ME=`readlink -f $0`; MYDIR=`dirname $ME`; cd $MYDIR
-
-tidy -mqiu *.htm
-tidy -mqiu *.html
--- promotes/linux/tidy.sh
+++
@@ -1,4 +0,0 @@
-# if called from elsewhere, make sure we start here
-ME=`readlink -f $0`; MYDIR=`dirname $ME`; cd $MYDIR
-
-tidy -m *.html
