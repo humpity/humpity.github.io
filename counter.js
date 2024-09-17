a2c0a7ec32ef4ebeafecb81ad1a4d4866a1c375f
HmpSoft Admin <humpty@mail.com>
2024-09-18 06:01:43 +0800

    commit

--- counter.js
+++ counter.js
@@ -1,31 +1,18 @@
-document.write ('<HTML>\
-<HEAD>\
-  <META content="text/html; charset=us-ascii" http-equiv=\
-  "Content-Type">\
-  <META name="generator" content=\
-  "HTML Tidy for Linux/x86 (vers 25 March 2009), see www.w3.org">\
-  <TITLE>counter</TITLE>\
-  <BASE target="_parent">\
-</HEAD>\
-<BODY alink="#000099" link="#000099" vlink="#990099">\
-<!-- Start of StatCounter Code for Default Guide -->\
-<SCRIPT type="text/javascript">\
-	var sc_project=3241309; \
-        var sc_invisible=0; \
-        var sc_security="4e868b2d"; \
-		var scJsHost = "https://"; \
-</SCRIPT>\
-<SCRIPT type="text/javascript" src=\
-          "https://www.statcounter.com/counter/counter.js">\
-</SCRIPT>\
-<NOSCRIPT>\
-          <DIV class="statcounter">\
-            <A title="web analytics" href="https://statcounter.com/"\
-            target="_blank"><IMG class="statcounter" src=\
-            "https://c.statcounter.com/3241309/0/4e868b2d/0/" alt=\
-            "web analytics"></A>\
-          </DIV></NOSCRIPT> \
-<!-- End of StatCounter Code for Default Guide -->\
-</BODY>\
-</HTML>\
-');
+<!-- Default Statcounter code for humpty s corner
+http://humpity.github.io -->
+<script type="text/javascript">
+var sc_project=3241309; 
+var sc_invisible=0; 
+var sc_security="4e868b2d"; 
+var scJsHost = "https://";
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="Web Analytics
+Made Easy - Statcounter" href="https://statcounter.com/"
+target="_blank"><img class="statcounter"
+src="https://c.statcounter.com/3241309/0/4e868b2d/0/"
+alt="Web Analytics Made Easy - Statcounter"
+referrerPolicy="no-referrer-when-downgrade"></a></div></noscript>
+<!-- End of Statcounter Code -->
